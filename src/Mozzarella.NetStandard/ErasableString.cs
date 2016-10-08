ecbf1174e31b0a333604159ed1ceac2ca3753b19
Troy Willmot <yort@yortondotnet.com>
2016-10-08 17:30:24 +1300

    Changed NetStandard library to target 1.1. Added ErasableString and tests for NetStandard11. Updates to nusepc etc.

---
+++ src/Mozzarella.NetStandard/ErasableString.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Mozzarella
+{
+	/// <summary>
+	/// Represents a string that is pinned in memory and can be overwritten when finished with, limiting the time where the stored value can be obtained.
+	/// </summary>
+	/// <remarks>
+	/// <para>In theory this improves (but does not guarantee) security by reducing the amount of time an unencrypted string value is available in memory. No guarantees or promises are made about the security provided by this class.</para>
+	/// <para>Note, failure to explicitly dispose this class will reduce/eliminate it's effectiveness. <see cref="Dispose"/> should be called as early as possible, but no earlier.</para>
+	/// <para>Available only in projects using the .Net Standard 1.1 or later Mozzarella assembly, NOT supported in the 'portable' version.</para>
+	/// </remarks>
+	public sealed class ErasableString : IDisposable
+	{
+
+		#region Fields
+
+		private readonly string _Value;
+		private object _Synchroniser;
+		private GCHandle _ValuePinnedHandle;
+		private bool _IsDisposed;
+		private bool _IsCleared;
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Full constructor.
+		/// </summary>
+		/// <param name="value">The .Net string to manage, used as the return value for the <see cref="Value"/> property.</param>
+		public ErasableString(string value)
+		{
+			_Synchroniser = new object();
+			_Value = value;
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Returns true if the <see cref="Dispose"/> method has been called.
+		/// </summary>
+		public bool IsDisposed
+		{
+			get { return _IsDisposed; }
+		}
+
+		/// <summary>
+		/// Returns true if the <see cref="Clear"/> method has been called.
+		/// </summary>
+		public bool IsCleared
+		{
+			get { return _IsCleared; }
+		}
+
+		/// <summary>
+		/// Returns the value of the string. This will be a string of character zero, for the length of the original string, if it has been cleared.
+		/// </summary>
+		public string Value
+		{
+			get
+			{
+				if (IsDisposed) throw new ObjectDisposedException(nameof(ErasableString));
+
+				return _Value;
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Overwrites the string value contained by this instance with a series of character zero values.
+		/// </summary>
+		[System.Security.SecurityCritical]
+		public void Clear()
+		{
+			if (IsDisposed) throw new ObjectDisposedException(nameof(ErasableString));
+			if (IsCleared) return;
+
+			if (!String.IsNullOrEmpty(_Value))
+			{
+				lock (_Synchroniser)
+				{
+					if (!_IsCleared)
+					{
+						unsafe
+						{
+							fixed (char* p = Value)
+							{
+								for (int cnt = 0; cnt < Value.Length; cnt++)
+								{
+									char* p1 = p + cnt;
+									*p1 = (Char)0;
+								}
+							}
+						}
+					}
+				}
+			}
+
+			_IsCleared = true;
+		}
+
+		#endregion
+
+		#region IDisposable Members
+
+		/// <summary>
+		/// Disposes this instance.
+		/// </summary>
+		/// <remarks>
+		/// <para>Clears the string if the <see cref="Clear"/> method has not already been called, and unpins the wrapped string in memory.</para>
+		/// </remarks>
+		public void Dispose()
+		{
+			if (!_IsDisposed)
+			{
+				try
+				{
+					try
+					{
+						Clear();
+					}
+					finally
+					{
+						if (_ValuePinnedHandle.IsAllocated)
+							_ValuePinnedHandle.Free();
+					}
+				}
+				finally
+				{
+					_IsDisposed = true;
+				}
+			}
+		}
+
+		#endregion
+
+	}
+}
---
+++ src/Mozzarella.Tests/ErasableStringTests.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mozzarella.Tests
+{
+	[TestClass]
+	public class ErasableStringTests
+	{
+		[TestMethod]
+		public void ErasableString_Constructor_ConstructsWithNonBlankString()
+		{
+			using (var es = new ErasableString("test"))
+			{
+				Assert.AreEqual("test", es.Value);
+			}
+		}
+
+		[TestMethod]
+		public void ErasableString_Constructor_ConstructsWithNull()
+		{
+			using (var es = new ErasableString(null))
+			{
+			}
+		}
+
+		[TestMethod]
+		public void ErasableString_Constructor_ConstructsWithEmptyString()
+		{
+			using (var es = new ErasableString(String.Empty))
+			{
+			}
+		}
+
+		[TestMethod]
+		public void ErasableString_Clear_OverwritesString()
+		{
+			using (var es = new ErasableString("Test"))
+			{
+				Assert.AreEqual("Test", es.Value);
+				es.Clear();
+				Assert.AreEqual(new string((char)0, 4), es.Value);
+			}
+		}
+
+		[TestMethod]
+		public void ErasableString_Clear_SetsIsCleared()
+		{
+			using (var es = new ErasableString("Test"))
+			{
+				Assert.AreEqual("Test", es.Value);
+				es.Clear();
+				Assert.AreEqual(true, es.IsCleared);
+			}
+		}
+
+		[TestMethod]
+		public void ErasableString_Clear_DisposeClearsString()
+		{
+			var es = new ErasableString("Test");
+			es.Dispose();
+			Assert.AreEqual(true, es.IsCleared);
+		}
+
+		[ExpectedException(typeof(ObjectDisposedException))]
+		[TestMethod]
+		public void ErasableString_Clear_ThrowsWhenDisposed()
+		{
+			var es = new ErasableString("Test");
+			es.Dispose();
+			es.Clear();
+			Assert.AreEqual(true, es.IsCleared);
+		}
+
+		[ExpectedException(typeof(ObjectDisposedException))]
+		[TestMethod]
+		public void ErasableString_Value_ThrowsWhenDisposed()
+		{
+			var es = new ErasableString("Test");
+			es.Dispose();
+			var s = es.Value;
+		}
+
+		[TestMethod]
+		public void ErasableString_Dispose_CanDisposeSafelyMultipleTimes()
+		{
+			var es = new ErasableString("Test");
+			es.Dispose();
+			es.Dispose();
+		}
+
+	}
+}
